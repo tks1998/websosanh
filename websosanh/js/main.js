d377092226c99e5a21c49b83e690674bb4890e0f
Ry Sama <conlocxoaythuytrieu@gmail.com>
2019-05-24 09:18:31 +0700

    giao dien

---
+++ websosanh/js/main.js
@@ -0,0 +1,7 @@
+function abcxxxx() {
+	document.getElementById("tensp").innerHTML = "abc";
+	document.getElementById("nsxsp").innerHTML = "nha san xuat";
+	document.getElementById("giasp").innerHTML = "50000";
+	console.log("abc");
+	
+}
